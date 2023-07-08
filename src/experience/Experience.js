0ddbc32e7edb5b2d9c9cfa22774932f9995b8673
Dominic Clerici <dclerici77@gmail.com>
2023-07-07 22:08:36 -0700

    07/07

--- src/experience/Experience.js
+++ src/experience/Experience.js
@@ -5,7 +5,10 @@ const Experience = () => {
     return (
         <div className={styles.Experience}>
             <h1 className="header">Experience</h1>
-            <a>etc..</a>
+            <a>
+                Aliquyam ipsum stet lorem diam sit kasd stet. Justo voluptua tempor dolor kasd et, sadipscing sit eos rebum tempor, magna at tempor lorem sed accusam invidunt. Sanctus lorem tempor eos dolor elitr eirmod ipsum accusam, invidunt et diam ut takimata.Elitr invidunt sadipscing tempor sed dolor et rebum gubergren dolore ut. Sit consetetur ea accusam labore. Dolor et amet dolor. Aliquyam
+                ipsum stet lorem diam sit kasd stet. Justo voluptua tempor dolor kasd et, sadipscing sit eos rebum tempor, magna at tempor lorem sed accusam invidunt. Sanctus lorem tempor eos dolor elitr eirmod ipsum accusam, invidunt et diam ut takimata.Elitr invidunt sadipscing tempor sed dolor et rebum gubergren dolore ut. Sit consetetur ea accusam labore. Dolor et amet dolor.
+            </a>
         </div>
     )
 }
